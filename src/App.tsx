727bb6d2cc3c9cc275ccb5a305c7f0f8fce40e62
gabs <gabrielazbarreto@gmail.com>
2025-09-28 00:02:53 -0300

    add axios and react-query for API integration; set up environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { RouterProvider } from "react-router/dom";
 
 import { router } from "./routes";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import {  QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
